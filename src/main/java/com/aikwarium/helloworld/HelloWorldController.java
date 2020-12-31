ea607b30952da038b18530584ab11d2a2bb20069
Bhushan <bhushan.parikh@gmail.com>
2020-12-31 11:58:22 +0530

    service code added
    service code added

--- src/main/java/com/aikwarium/helloworld/HelloWorldController.java
+++ src/main/java/com/aikwarium/helloworld/HelloWorldController.java
@@ -3,13 +3,18 @@
  */
 package com.aikwarium.helloworld;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author Bhushan
  *
  */
-@Controller
+@RestController
 public class HelloWorldController {
 
+	@GetMapping(path = "/hello")
+	public String sayHello() {
+		return "Hello World!";
+	}
 }
